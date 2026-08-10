6e5d8bc023c7e72c08f8fcf2eb05b4c65282e1fb
GouveiaK <Kassiano.dev@gmail.com>
2026-08-10 18:59:05 -0300

    atualização da página criaturas

--- script.js
+++ script.js
@@ -7,4 +7,20 @@ listItems.forEach(item => {
 
         previewBox.style.backgroundImage = `url('${bgPath}')`;
     })
-})
+});
+
+
+let count = 1;
+document.getElementById("radio1").checked = true;
+
+setInterval(function () {
+    nextImage();
+}, 3000);
+
+function nextImage(){
+    count++;
+    if (count>5){
+        count = 1;
+    }
+    document.getElementById('radio' + count).checked = true;
+}
